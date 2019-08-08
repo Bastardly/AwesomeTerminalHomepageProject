1269286a3068711ba26214e77e8720db29250c00
Flemming Hansen <flemshans@gmail.com>
2019-08-08 23:27:53 +0200

    Cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,5 @@
 module.exports = {
     parser: "@typescript-eslint/parser",
-    // env: {
-    //     browser: true,
-    //     node: true,
-    //     es6: true,
-    //     commonjs: true,
-    // },
     extends: ["plugin:prettier/recommended", "prettier", "prettier/react"],
     plugins: ["@typescript-eslint/eslint-plugin", "prettier", "react-hooks", "prettier/react"],
     parserOptions: {
@@ -14,7 +8,6 @@ module.exports = {
         },
         ecmaVersion: 2018,
         sourceType: "module",
-        // "useJSXTextNode": true, // only false if eslint is v4
     },
     rules: {
         "prettier/prettier": "error",

cdefb6f4fededa70d352b32d78a3b0b8f177b17b
Flemming Hansen <flemshans@gmail.com>
2019-08-09 12:55:34 +0200

    Sketchup for ls functionality

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,13 +1,20 @@
-
-import React from 'react';
-import ContentWrapper from '../terminal/ContentWrapper';
+import React from "react";
+import ContentWrapper from "../terminal/ContentWrapper";
+import Link from "src/ui/Link";
 export default function Blog() {
     return (
-        <ContentWrapper
-            header="Blog"
-        >
-            <div>This is my blog</div>
-
+        <ContentWrapper header="Blog">
+            <div>
+                <div>
+                    <Link name="LatestBlogPosts" path="/" /> /
+                </div>
+                <div>
+                    <Link name="TopBlogPosts" path="/" /> /
+                </div>
+                <div>
+                    <Link name="Something else" path="/" /> /
+                </div>
+            </div>
         </ContentWrapper>
-    )
+    );
 }
--- src/ui/Link.tsx
+++ src/ui/Link.tsx
@@ -1,18 +1,22 @@
-import React, { useContext, useCallback } from 'react';
-import { getRouteData } from 'src/router';
-import { appContext } from 'src/App';
+import React, { useContext, useCallback } from "react";
+import { getRouteData } from "src/router";
+import { appContext } from "src/App";
 
 interface LinkProps {
-    name: string,
-    path: string,
-    hash?: string,
-    query?: string,
-    disabled?: boolean,
+    name: string;
+    path: string;
+    hash?: string;
+    query?: string;
+    disabled?: boolean;
 }
 
-export default function Link({name, path, hash, query, disabled}: LinkProps) {
-    const { changeRouteData } = useContext(appContext)
-    const handleClick = useCallback(() => changeRouteData(getRouteData(path, hash, query)), [path, hash, query])
+export default function Link({ name, path, hash, query, disabled }: LinkProps) {
+    const { changeRouteData } = useContext(appContext);
+    const handleClick = useCallback(() => changeRouteData(getRouteData(path, hash, query)), [path, hash, query]);
 
-    return <button onClick={handleClick} disabled={disabled}>{name}</button>
+    return (
+        <button onClick={handleClick} disabled={disabled}>
+            {name}
+        </button>
+    );
 }
